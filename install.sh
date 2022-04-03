e41044eea202c237ba3eec0c6a9fe9be9a1c0733
Mostakim Jihad <mostakimjihad652001@gmail.com>
2022-04-03 22:13:06 +0600

    install.sh added

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+echo "changing mode ......."
+chmod +x kamoyi.sh
+chmod +x tmp.sh
+echo "mode change done"
+echo "installing dependeces............"
+echo "please wait..."
+sudo apt install figlet lolcat
